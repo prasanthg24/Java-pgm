9956b0ce0209bfd3366d24686de84b3d063c3046
Gowtham R <83116045+Gowtham-05@users.noreply.github.com>
2022-09-03 19:55:08 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class array1{
+   public static void main(String args[])
+   {
+      int num[] = {10,20,30};
+	  for(int n : num)
+	  {
+	   System.out.println(n);
+	  }
+   }
+}
